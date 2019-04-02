6cd57eb1928835240ae1ab2e4e2bef47d1043d7a
Borxes <yurasherman+github@gmail.com>
2019-04-02 18:56:33 +0300

    remove extra comments

--- services/tgService.js
+++ services/tgService.js
@@ -3,15 +3,6 @@ const keys = require('../config/keys');
 
 const telegram = new Telegram(keys.telegramToken);
 
-// telegram
-//         .sendMessage('@makerdaoevents', message, { parse_mode: 'Markdown' })
-//         .then(data => {
-//           console.log(`Poasting ${message}`);
-//         })
-//         .catch(err => {
-//           console.log(`Telegram error: ${err}`);
-//         });
-
 const postMessage = async message => {
   console.log(`tgService trying to post ${message}`);
   return telegram.sendMessage('@hotcryptotwitter', message, {
