22ede3df21e3e5c60f9bc5e71eb4a7f8da74940f
Borxes <yurasherman+github@gmail.com>
2019-04-22 15:21:57 +0300

    uncomment nomics tests

--- tests/nomics.test.js
+++ tests/nomics.test.js
@@ -4,46 +4,46 @@ const moment = require('moment');
 
 const TIMEOUT = 10000;
 
-// test(
-//   'currencies',
-//   async () => {
-//     const currencies = await nomics.getCurrencies();
-//     console.log(currencies.slice(0, 5));
-//     expect(currencies).toEqual(
-//       expect.arrayContaining([{ id: 'BTC' }, { id: 'ZEN' }])
-//     );
-//   },
-//   TIMEOUT
-// );
+test(
+  'currencies',
+  async () => {
+    const currencies = await nomics.getCurrencies();
+    console.log(currencies.slice(0, 5));
+    expect(currencies).toEqual(
+      expect.arrayContaining([{ id: 'BTC' }, { id: 'ZEN' }])
+    );
+  },
+  TIMEOUT
+);
 
-// test(
-//   'prices',
-//   async () => {
-//     const prices = await nomics.getPrices();
-//     console.log(prices.slice(0, 5));
-//     expect(prices[0].currency).toBe('18C');
-//   },
-//   TIMEOUT
-// );
+test(
+  'prices',
+  async () => {
+    const prices = await nomics.getPrices();
+    console.log(prices.slice(0, 5));
+    expect(prices[0].currency).toBe('18C');
+  },
+  TIMEOUT
+);
 
-// test('getTickerPrice', async () => {
-//   const ethPrice = await nomics.getTickerPrice('ICX');
-//   console.log(ethPrice);
-//   expect(ethPrice).toHaveProperty('usdPrice');
-//   expect(typeof ethPrice.usdPrice).toBe('number');
-//   expect(ethPrice).toHaveProperty('btcPrice');
-//   expect(ethPrice.btcPrice).not.toBeNaN();
-//   const nonPrice = await nomics.getTickerPrice('QWERTY');
-//   expect(nonPrice.btcPrice).toBe(0);
-// });
+test('getTickerPrice', async () => {
+  const ethPrice = await nomics.getTickerPrice('ICX');
+  console.log(ethPrice);
+  expect(ethPrice).toHaveProperty('usdPrice');
+  expect(typeof ethPrice.usdPrice).toBe('number');
+  expect(ethPrice).toHaveProperty('btcPrice');
+  expect(ethPrice.btcPrice).not.toBeNaN();
+  const nonPrice = await nomics.getTickerPrice('QWERTY');
+  expect(nonPrice.btcPrice).toBe(0);
+});
 
-// test('getCurrenciesInterval', async () => {
-//   const prices1hr = await nomics.getCurrenciesInterval(
-//     moment().subtract(1, 'hours')
-//   );
-//   expect(prices1hr).toBeDefined();
-//   console.log(prices1hr.slice(2, 5));
-// });
+test('getCurrenciesInterval', async () => {
+  const prices1hr = await nomics.getCurrenciesInterval(
+    moment().subtract(1, 'hours')
+  );
+  expect(prices1hr).toBeDefined();
+  console.log(prices1hr.slice(2, 5));
+});
 
 test(
   'getTickersPriceChange',
